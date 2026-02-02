4b719db05fb872fe5e07e8cf7684bc81560ff241
yunjindev <hunterweisenbach@me.com>
2026-02-02 14:43:04 -0500

    Chapter 2

--- main.go
+++ main.go
@@ -1,2 +1,24 @@
-test
-updated test<F2>
+package main
+
+import "fmt"
+
+func main() {
+
+	type Saiyan struct {
+		Name string
+		Power int
+	}
+		
+	goku := Saiyan {
+		Name: "Goku",
+		Power: 9000,
+	}
+
+	taiki := Saiyan {
+		Name: "Taiki",
+		Power: 1000,
+	}
+	
+	fmt.Printf("%v\n %v\n",goku, taiki)
+
+}
